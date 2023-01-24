20df4514548c1bcd0f45721eb411b6dd6cd9bafe
CarloColletti~ <carlocolletti5@gmail.com>
2023-01-24 15:35:33 +0100

    aggiunti primi comandi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// quanti chilometri deve fare l'utente
+const km_input = document.getElementById("km_input");
+console.log("La distanza da percorrere è: ", km_input.value);
+
+// quanti anni ha l'utente ?
+const age_input = document.getElementById("age_input");
+console.log("L'età del passeggero è: ", age_input.value);
+
+// quanto costa il biglietto normalmente
+const cost_biglietto = 0.21;
+
+// tasto per calcolo costo 
+const button_submit = document.getElementById("button_submit")
+
